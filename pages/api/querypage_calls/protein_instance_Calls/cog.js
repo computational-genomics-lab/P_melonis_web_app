c8467958e1a60f649872bc0d6840cd92080804d8
Aditya-Upa <adityaupadhyay1997@gmail.com>
2024-11-06 17:53:39 +0530

    protein instance queries added

--- pages/api/querypage_calls/protein_instance_Calls/cog.js
+++ pages/api/querypage_calls/protein_instance_Calls/cog.js
@@ -5,8 +5,13 @@ export default function handler(req, res) {
 
 //fetch all the COG ids for a particular strain of an organism (denoted by its taxonomy ID)
 
-const query = `SELECT COG, prediction_id FROM COG WHERE taxonomy_id=${taxon_id} AND strain_number=${strain_number}`;
+//const query = `SELECT COG, prediction_id FROM COG WHERE taxonomy_id=${taxon_id} AND strain_number=${strain_number}`;
 
+const query = `SELECT c.COG, c.prediction_id, pr.name AS gene_name, nl.start_min, 
+nl.end_min, ena.source_ID AS scaffold, ena.sequence AS external_sequence FROM COG c JOIN protein pr ON 
+c.protein_instance_id = pr.protein_ID JOIN geneinstance gi ON pr.gene_instance_ID = gi.gene_instance_ID 
+JOIN nalocation nl ON gi.na_feature_ID = nl.na_feature_ID JOIN nafeatureimp nf ON gi.na_feature_ID = nf.na_feature_ID 
+JOIN externalnasequence ena ON nf.na_sequence_ID = ena.na_sequence_ID WHERE c.taxonomy_id = ${taxon_id} AND c.strain_number = ${strain_number}`;
   pool.query(query, (error, results) => {
     if (error) {
       res.status(500).json({ error});
