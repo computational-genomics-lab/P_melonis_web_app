632ec5e6f9d9c1329464797906db70ad486a0a9a
Aditya-Upa <adityaupadhyay1997@gmail.com>
2024-11-11 18:14:41 +0530

    parameterized queries to prevent SQL injection

--- pages/api/querypage_calls/protein_instance_Calls/cog.js
+++ pages/api/querypage_calls/protein_instance_Calls/cog.js
@@ -10,8 +10,12 @@ export default function handler(req, res) {
 const query = `SELECT c.COG, c.prediction_id, nf.name as transcipt, pr.name AS gene_name, ena.source_ID AS scaffold FROM COG c JOIN protein pr ON 
 c.protein_instance_id = pr.protein_ID JOIN geneinstance gi ON pr.gene_instance_ID = gi.gene_instance_ID 
 JOIN nafeatureimp nf ON gi.na_feature_ID = nf.na_feature_ID 
-JOIN externalnasequence ena ON nf.na_sequence_ID = ena.na_sequence_ID WHERE c.taxonomy_id = ${taxon_id} AND c.strain_number = ${strain_number}`;
-  pool.query(query, (error, results) => {
+JOIN externalnasequence ena ON nf.na_sequence_ID = ena.na_sequence_ID WHERE c.taxonomy_id = ? 
+AND c.strain_number = ?`;
+
+const params = [taxon_id, strain_number];
+
+  pool.query(query, params, (error, results) => {
     if (error) {
       res.status(500).json({ error});
     } else {
