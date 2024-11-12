2c102b58c1361dde31b33356b3172d69cd52a9b5
Aditya-Upa <adityaupadhyay1997@gmail.com>
2024-11-12 17:26:19 +0530

    scaffold api

--- pages/api/statistics_calls/scaffold_details.js
+++ pages/api/statistics_calls/scaffold_details.js
@@ -6,11 +6,11 @@ export default function handler(req, res) {
   const { taxon_id, strain_number} = req.query; //accepts taxon_ID of particular organism
 
 const query = `select source_ID AS scaffold_name,length from externalnasequence where 
-sequence_type_ID=1 and taxon_ID=${taxon_id} and strain_number=${strain_number} order by length desc`;
+sequence_type_ID=1 and taxon_ID=? and strain_number=? order by length desc`;
   
 const params = [taxon_id, strain_number];
 
-pool.query(query, (error, results) => {
+pool.query(query, params, (error, results) => {
     if (error) {
       res.status(500).json({ error });
     } else {
