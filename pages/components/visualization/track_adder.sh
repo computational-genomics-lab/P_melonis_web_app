00535e02ec75fbd4f39429d2ad149092ffae8fc4
Aditya-Upa <adityaupadhyay1997@gmail.com>
2024-12-03 12:44:56 +0530

    modified: track_adder.sh

--- pages/components/visualization/track_adder.sh
+++ pages/components/visualization/track_adder.sh
@@ -1,10 +1,10 @@
 #this was done to generate the config file for oomycetes data using the jbrowse CLI 
 
-for file in ../../../public/genomes/test/*.fna ; do
+for file in ../../../public/genomes/*.fna ; do
 	b=`basename $file .fna`
 	echo $file, $b
 	jbrowse add-assembly $file --load inPlace
-	jbrowse add-track ../../../public/genomes/test/"$b"_with_product_name.sorted.gff3.gz --load inPlace --assemblyNames $b.fna
-	jbrowse add-track ../../../public/genomes/test/"$b"_rxlr.bw --load inPlace --assemblyNames $b.fna
+	jbrowse add-track ../../../public/genomes/"$b"_with_product_name.sorted.gff3.gz --load inPlace --assemblyNames $b.fna
+	jbrowse add-track ../../../public/genomes/"$b"_rxlr.bw --load inPlace --assemblyNames $b.fna
 
 done
