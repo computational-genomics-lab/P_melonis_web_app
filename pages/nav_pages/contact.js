96a31b41133e73c66352b3577a529dd40576f88f
Aditya Upadhyay <83017903+Aditya-Upa@users.noreply.github.com>
2025-02-06 18:27:58 +0530

    Update contact.js
    dynamic routing (opens the jbrowse tab in new window)

--- pages/nav_pages/contact.js
+++ pages/nav_pages/contact.js
@@ -1,18 +1,21 @@
-//more or less redundant because it is not the main jbrowse script
-import React from "react";
-// import View from "../components/visualization/test_jbrowse";
-import View from "../components/visualization/test_jbrowse";
+import dynamic from 'next/dynamic';
+import React from 'react';
+
+// Use Next.js dynamic import for code-splitting so that the heavy View component
+// is loaded only when this page is visited.
+const View = dynamic(() => import('../components/visualization/test_jbrowse'), {
+  loading: () => <p>Loading JBrowse2 view...</p>,
+});
 
 const Contact = () => {
-return(
+  return (
     <div>
-{/* <h2> Currently only one organism has been loaded in jbrowse </h2> */}
-{/* <View name='Phyag_NZFS3770'/> */}
-<h3> <i>Phytophthora melonis</i> genomes visualized using JBrowse2</h3>
-<View />
-{/* this was done because there are a lot of features which aren't available in the linear genome
-view of JBrowse2 */}
-{/*<iframe src="" width="100%" height="1000vh"/>*/}
-</div>)
-}
-export default Contact
+      <h3>
+        <i>Phytophthora melonis</i> genomes visualized using JBrowse2
+      </h3>
+      <View />
+    </div>
+  );
+};
+
+export default Contact;
