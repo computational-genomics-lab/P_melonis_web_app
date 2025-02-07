fc54bcde79bd089baa114d92acbe524c24907216
Aditya Upadhyay <83017903+Aditya-Upa@users.noreply.github.com>
2025-02-07 15:11:46 +0530

    Update navbar.js
    styling changes

--- pages/navbar.js
+++ pages/navbar.js
@@ -3,35 +3,52 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 
-  function Navbar() {
+function Navbar() {
+  const mystyle = {
+    fontFamily: 'Sans-serif',
+    padding: '20px',
+    fontSize: '35px',
+    marginBottom: '30px',
+  };
 
-    const mystyle = {
-      fontFamily: 'Sans-serif', padding: '20px', fontSize: '35px', marginBottom: '30px'
-    }
-    return (
-   <>
-        <nav style= {mystyle} className={styles.mainnav}>
-
-            <ul>
-      
-          <Link href="/"><li>Home</li></Link>
-         <Link href="/nav_pages/query" legacyBehavior>
-         <a target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>Query </a></Link>
-         <Link href="/nav_pages/statistics" legacyBehavior>
-         <a target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
-          Statistics</a></Link>
-      <Link href="/nav_pages/tools" legacyBehavior>
-      <a target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>Tools</a></Link>
-        <Link href="/nav_pages/contact" legacyBehavior> 
-      <a target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+  return (
+    <nav style={mystyle} className={styles.mainnav}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/nav_pages/query" legacyBehavior>
+            <a target="_blank" rel="noopener noreferrer">
+              Query
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/nav_pages/statistics" legacyBehavior>
+            <a target="_blank" rel="noopener noreferrer">
+              Statistics
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/nav_pages/tools" legacyBehavior>
+            <a target="_blank" rel="noopener noreferrer">
+              Tools
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/nav_pages/contact" legacyBehavior>
+            <a target="_blank" rel="noopener noreferrer">
               JBrowse2
             </a>
           </Link>
-         
-          </ul>       
-      </nav>
-      
-      </>
-    )}
-  
-export default Navbar
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
+
