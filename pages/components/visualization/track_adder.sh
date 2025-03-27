a41e4f328ddf70571247d08f62a7788987dab937
Aditya Upadhyay <83017903+Aditya-Upa@users.noreply.github.com>
2025-03-27 12:19:03 +0530

    Update track_adder.sh
    removed unnecessary basename string from gff3 file

--- pages/components/visualization/track_adder.sh
+++ pages/components/visualization/track_adder.sh
@@ -4,7 +4,7 @@ for file in ../../../public/genomes/*.fna ; do
 	b=`basename $file .fna`
 	echo $file, $b
 	jbrowse add-assembly $file --load inPlace
-	jbrowse add-track ../../../public/genomes/"$b"_with_product_name.sorted.gff3.gz --load inPlace --assemblyNames $b.fna
+	jbrowse add-track ../../../public/genomes/"$b".sorted.gff3.gz --load inPlace --assemblyNames $b.fna
 	jbrowse add-track ../../../public/genomes/"$b"_rxlr.bw --load inPlace --assemblyNames $b.fna
 
 done
