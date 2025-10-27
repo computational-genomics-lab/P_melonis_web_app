b0648b4b2e7f0808c44db0bab0f62bb31488d932
Aditya Upadhyay <83017903+Aditya-Upa@users.noreply.github.com>
2025-10-27 15:26:18 +0530

    Update config.js
    aesthetic changes

--- pages/components/visualization/config.js
+++ pages/components/visualization/config.js
@@ -152,6 +152,26 @@ const getConfig = (props) => {
               },
             },
           ],
+  configuration: {
+      "theme" :{
+        "typography": { "fontSize": 22 },
+      "spacing": 2,
+        "palette": {
+          "primary": {
+            "main": "#311b92"
+          },
+          "secondary": {
+            "main": "#0097a7"
+          },
+          "tertiary": {
+            "main": "#f57c00"
+          },
+          "quaternary": {
+            "main": "#d50000"
+          }
+        }
+      }
+    },
     tracks: [
       {
         type: `FeatureTrack`,
